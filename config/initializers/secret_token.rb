957498050e892a7847e21f816e8c2b1944a0ebbf
Quang Pham <me@quangpham.com>
2013-05-01 12:33:04 +0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tumblranalytic::Application.config.secret_token = '4729f8b45db0fcaba0277582b65c565664bdbf3dce87971aaa2b0b8c1e67bef97a6af60c715e7b23db315b877ed908a992a0901dc9b49c7323ed35add88b5e61'
